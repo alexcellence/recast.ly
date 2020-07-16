f809c9aba17a4510410f4a3c8f3cd175d6fa46be
Erfanch <erfan.chinif@gmail.com>
2020-07-16 09:42:36 -0700

    VideoPlayer solved

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,20 +1,9 @@
 import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => (
-  // console.log('videos', props)
-  // console.log(props)
-  // return (
   <div className="video-list">
-    {/* <div><h5><VideoListEntry video={props.videos[0]}/></h5></div>
-    <div><h5><VideoListEntry video={props.videos[1]}/></h5></div>
-    <div><h5><VideoListEntry video={props.videos[2]}/></h5></div>
-    <div><h5><VideoListEntry video={props.videos[3]}/></h5></div>
-    <div><h5><VideoListEntry video={props.videos[4]}/></h5></div> */}
-
-    <div><h5>{props.videos.map(video => <VideoListEntry  video={video}/>)}</h5></div>
-    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
+    {props.videos.map(video => <VideoListEntry  video={video}/>)}
   </div>
-  // )
 );
 
 // PropTypes tell other developers what `props` a component expects
