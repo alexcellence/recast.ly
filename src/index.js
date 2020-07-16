cd12d56e4b6ba59add518b2b1f7a47e5c3e9ac8f
Erfanch <erfan.chinif@gmail.com>
2020-07-15 22:32:06 -0700

    Passed videoListEntry tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-// TODO: Render the `App` component to the DOM
+// // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
 
+ReactDOM.render(<App />, document.getElementById('app'));
